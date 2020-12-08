ced205e27b03eb09ed88a6ed948c6874e216b12d
olga-kuriatnyk <olhak@uw.edu>
2020-12-08 14:35:42 -0800

    Update BSTree.cpp

--- Program5/BSTree.cpp
+++ Program5/BSTree.cpp
@@ -1,7 +1,11 @@
+#ifndef BSTREE_CPP_
+#define BSRTREE_CPP_
+
 #include "BSTree.h"
 
 BSTree::BSTree()
 {
+	root_ = NULL;
 }
 
 BSTree::~BSTree()
@@ -9,6 +13,68 @@ BSTree::~BSTree()
 	Empty();
 }
 
-void BSTree::Empty() // TODO: implement
+bool BSTree::Insert(Account* account_ptr)
 {
+	int insert_account_id = account_ptr->getAccountID();
+	if (insert_account_id < 1000 || insert_account_id > 9999)
+	{
+		cout << "ERROR: Account Number Not Valid" << endl;
+		return false;
+	}
+
+	if (root_ == NULL)
+	{
+		root_ = new Node;
+		root_->account_ptr_ = account_ptr;
+		root_->left_ = NULL;
+		root_->right_ = NULL;
+		return true;
+	}
+
+	else
+	{
+		Node* current = root_;
+		// TODO: implement Recursive Insert ? 
+
+	}
+	return false;
 }
+
+bool BSTree::Retrieve(const int& account_id, Account*& account_ptr) const
+{
+	Node* current = root_;
+	bool search = false;
+
+	while (!search)
+	{
+		if ((current != NULL) && (account_id == current->account_ptr_->getAccountID())) //found the account_id 
+		{
+			search = true;
+			account_ptr = current->account_ptr_;
+			return search;
+		}
+		else if ((current != NULL) && (account_id > current->account_ptr_->getAccountID()))
+		{
+			current = current->right_;
+		}
+		else if ((current != NULL) && (account_id < current->account_ptr_->getAccountID()))
+		{
+			current = current->left_;
+		}
+		//else
+		//{
+		//	search = true;
+		//}
+	}
+
+	cout << "ERROR: Account Number " << account_id << " not found." << endl;
+	return search;
+}
+
+void BSTree::Empty() // TODO: test
+{
+	delete root_;
+	root_ = NULL;
+}
+
+#endif
