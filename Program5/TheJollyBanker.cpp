455cae2c2461a66b8ac67c45f9884654e4c1b913
olga-kuriatnyk <olhak@uw.edu>
2020-12-08 21:59:38 -0800

    UpdateTheJollyBanker

--- Program5/TheJollyBanker.cpp
+++ Program5/TheJollyBanker.cpp
@@ -3,39 +3,50 @@
 
 #include <iostream>
 #include "Bank.h" 
+//#include "Account.h"
+//#include "Transaction.h"
 
 int main()
 {
-   // Bank jolly_bank; 
-   //jolly_bank.ProcesTransactions(); // execute transactions
-   //cout << jolly_bank << endl; // print the final balanses 
+	Bank jolly_bank;
+	jolly_bank.ReadTransactionsFromTheFile();
+	jolly_bank.ProcesTransactions(); // execute transactions
+	jolly_bank.Display();
+
+	//cout << jolly_bank << endl; // print the final balanses 
 
 
-    char chr = 'O';
-    char chr2 = 'D';
-    char chr3 = 'T';
-    char chr4 = 'W';
 
-    string ln = "Kur";
-    string fn = "Olga";
-    int account = 1234;
-    int fund = 1;
-    int amount = 1000;
+	 //char chr = 'O';
+	 //char chr2 = 'D';
+	 //char chr3 = 'T';
+	 //char chr4 = 'W';
 
-    int transfer_account_id = 4321;
-    int transfer_fund_id = 9;
-    string fail = "fail";
+	 //string ln = "Kur";
+	 //string fn = "Olga";
+	 //int account = 1234;
+	 //int fund = 1;
+	 //int amount = 1000;
 
-    Transaction trans(chr, ln, fn, account);
-    cout << trans << endl;
-    Transaction trans2(chr2, account, fund, amount);
-    cout << trans2 << endl;
+	 //int transfer_account_id = 4321;
+	 //int transfer_fund_id = 9;
+	 //string fail = "fail";
 
-    Transaction trans3(chr3, account, fund, amount, transfer_account_id, transfer_fund_id, fail);
-    cout << trans3 << endl;
-    Transaction trans4(chr4, account, fund, amount, fail);
-    cout << trans4 << endl;
+	 ////PRINT TRANSACTIONS
+	 //Transaction trans(chr, ln, fn, account);
+	 //cout << trans << endl;
+	 //Transaction trans2(chr2, account, fund, amount);
+	 //cout << trans2 << endl;
+
+	 //Transaction trans3(chr3, account, fund, amount, transfer_account_id, transfer_fund_id, fail);
+	 //cout << trans3 << endl;
+	 //Transaction trans4(chr4, account, fund, amount, fail);
+	 //cout << trans4 << endl;
+
+	 //Account accnt(fn, ln, account);
 
 
-    std::cout << "Hello World!\n";
+
+
+	std::cout << "Hello World!\n";
 }
