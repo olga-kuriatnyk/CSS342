12f5c6d3fc1f99a2b75f8f602e5ac23cfbb0f8c8
olga-kuriatnyk <olhak@uw.edu>
2021-01-10 17:22:19 -0800

    TheJollyBanker final changes

--- Program5/TheJollyBanker.cpp
+++ Program5/TheJollyBanker.cpp
@@ -1,16 +1,30 @@
-// TheJollyBanker.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//  Olga Kuraitnyk
+//  CSS342A
+//  12/10/2020
+//  Program 5: The Jolly Banker
+//  This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
 #include "Bank.h" 
+#include "BSTree.h"
 
 int main()
 {
 	Bank jolly_bank;
-	jolly_bank.ReadTransactionsFromTheFile();
-	jolly_bank.ProcesTransactions(); // execute transactions
-	jolly_bank.Display();
+	jolly_bank.ReadTransactionsFromTheFile(); // read
+	jolly_bank.ProcesTransactions(); // execute 
+	jolly_bank.Display(); // print
 
+	//BSTree my_bst;
+	//my_bst = jolly_bank.accounts_list;
+	//cout << "BSTREE COPY" << endl;
+	//my_bst.Display();
+	//jolly_bank.Display();
 
-	std::cout << "Hello World!\n";
+	cout << "Empty trees " << endl;
+	BSTree bst1;
+	jolly_bank.accounts_list = bst1;
+	bst1.Display();
+	jolly_bank.accounts_list.Display();
+
 }
